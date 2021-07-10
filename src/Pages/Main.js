32f0300132b7f5acd16c95d4efdee561e862c13a
Willem Jacobs <wamj58@gmail.com>
2021-07-10 17:27:56 -0500

    Progress main parts completed

---
+++ src/Pages/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import HornedBeast from '../Pages/HornedBeast'
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <HornedBeast title="Unicorn" description="Mythical single horn animal looks like a horse" img=""/>
+        <HornedBeast title="Unicorn" description="Mythical single horn animal looks like a horse" img=""/>
+      </main>
+    )
+  }
+}
+
+export default Main;
