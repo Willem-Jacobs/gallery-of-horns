1f104a8aa6b6e4f9301a95e08b909f8fbac95282
Willem Jacobs <wamj58@gmail.com>
2021-07-10 19:13:29 -0500

    Final completed

--- src/Pages/HornedBeast.js
+++ src/Pages/HornedBeast.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
+
+import "./HornedBeast.css";
 
 class HornedBeast extends React.Component {
   render() {
     return (
       <section>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.title} />
+        <p>{`Number of horns: ${this.props.horns}`}</p>
+        <img
+          src={this.props.imageUrl}
+          alt={this.props.title}
+          title={this.props.title}
+        />
         <p>{this.props.description}</p>
       </section>
-    )
+    );
   }
 }
 
